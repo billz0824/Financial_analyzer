8257dbe511385f9a2a2a9075cdc6190c6131345b
billz0824 <143441179+billz0824@users.noreply.github.com>
2025-03-28 22:25:40 -0400

    Update info.py

--- data_retrieval/info.py
+++ data_retrieval/info.py
@@ -1,3 +1,3 @@
-API_KEY = "AIzaSyBG7QvjkP6C88cyBuaKU2Pq-5J5ioY_PRo"
-EMAIL = "youranzhu123@gmail.com"
-HEADERS = {"User-Agent": "Northwestern University (Billzhu2027@u.northwestern.edu)"}
+API_KEY = # Insert your gemini API key
+EMAIL = # Your email
+HEADERS = # Request header to bypass auto blockers
